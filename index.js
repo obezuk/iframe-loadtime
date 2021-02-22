e3437b9028bf2132f777a19db4a1a7d151dc85f2
Tim Obezuk <501943+obezuk@users.noreply.github.com>
2021-02-22 14:05:58 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+var start = 0;
+
+var defaultURL = 'https://httpbin.org/anything';
+
+document.getElementById("url").value = defaultURL
+
+var iframe = document.createElement('iframe');
+
+iframe.onload = function() {
+    var end = Date.now();
+    document.getElementById("time").textContent = (end - start) + 'ms';
+}
+
+iframe.src = 'https://httpbin.org/anything';
+start = Date.now();
+
+document.body.appendChild(iframe);
+
+document.querySelector("#addressbar").addEventListener("submit", function(e) {
+    e.preventDefault();
+    var address = document.getElementById("url").value;
+    iframe.src = address;
+    start = Date.now();
+    return false;
+});
